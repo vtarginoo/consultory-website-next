a76a1733004c520db80c948573d50f540b4e988d
Victor Melentovytch <victor-melentovytch@victor.melentovytch>
2025-10-14 15:08:20 -0300

    fix bug image

--- components/PartnersSection.tsx
+++ components/PartnersSection.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import { useState } from 'react';
 import MoreButton from './MoreButton'; // Ajuste o caminho conforme necessário
 import { TeamMember } from '@/types/ITeamMember';
@@ -11,25 +10,25 @@ const teamMembers: TeamMember[] = [
     {
         name: 'Marcos Saraiva',
         role: 'Co-Founder & Chairman',
-        image: '/partners/partner-1.png',
+        image: '/employee/partner-1.png',
         resume: "Mais de 35 anos em liderança global nos setores de mineração, oil&gas, energia e agronegócio. Mestre em Marketing e Engenheiro Elétrico, com especializações no MIT, IMD, INSEAD/FDC e Kellogg."
     },
     {
         name: 'Rodrigo Colombaretti',
         role: 'Co-Founder & CEO',
-        image: '/partners/partner-2.png',
+        image: '/employee/partner-2.png',
         resume: "Executivo com 25 anos em multinacionais de grande porte, especialista em estratégia, logística, finanças e planejamento. Engenheiro Químico e advogado, com formações no MIT, IMD e COPPEAD."
     },
     {
         name: 'Vinícius Aquino',
         role: 'Co-Founder & COO',
-        image: '/partners/partner-3.png',
+        image: '/employee/partner-3.png',
         resume: "20 anos de experiência em consultoria, redução de custos e excelência operacional. Atuou em projetos que geraram mais de R$ 1 bilhão em resultados. Administrador pela PUC-RJ, com passagens por KPMG e BrainNet."
     },
     {
         name: 'Marcos Nicolas',
         role: 'Co-Founder & Advisor',
-        image: '/partners/partner-4.png',
+        image: '/employee/partner-4.png',
         resume: "Executivo com 38 anos de atuação nos setores privado e público, liderando projetos em infraestrutura, governo e tecnologia. Matemático e Analista de Sistemas pela UERJ, com MBA pelo COPPEAD/UFRJ."
     }
 ];
